f61e5319d010dd7a58c6ff7ace171834c0ebee31
Prideland-Okoi <prideland.okoi@gmail.com>
2024-02-10 15:03:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,142 +1,29 @@
 from flask import Flask, render_template
+from models import Member, Contribution, Expenses
 
 app = Flask(__name__)
 
-members_data = {
-    "Mr Albert": {
-        "full_name": "Mr Albert Odama",
-        "department": "Quality Control",
-        "company": "Royal Salt Limited",
-        "contributions": {"May": 6000, "June": 6000, "July": 6000, "August": 6000, "September": 6000, "October": 6000, "November": 6000, "December": 6000},
-        "total_contribution": 48000
-    },
-    "Mrs Jacinta": {
-        "full_name": "Mrs Jacinta Ogbonna",
-        "department": "Quality Control",
-        "company": "Royal Salt Limited",
-        "contributions": {"May": 6000, "June": 6000, "July": 6000, "August": 6000, "September": 6000, "October": 6000, "November": 6000, "December": 6000},
-        "total_contribution": 48000
-    },
-    "Mr Omini": {
-        "full_name": "Mr Omini Okoi",
-        "department": "Quality Control",
-        "company": "Royal Salt Limited",
-        "contributions": {"May": 6000, "June": 6000, "July": 6000, "August": 6000, "September": 6000, "October": 6000, "November": 6000, "December": 6000},
-        "total_contribution": 48000
-    },
-    "Mrs Henrietta": {
-        "full_name": "Mrs Henrietta Elijuoke",
-        "department": "Administration",
-        "company": "Atlantic Shrimpers Limited",
-        "contributions": {"May": 6000, "June": 6000, "July": 6000, "August": 6000, "September": 6000, "October": 6000, "November": 6000, "December": 6000},
-        "total_contribution": 48000
-    },
+def calculate_total_contribution(member):
+    return sum([contribution.amount for contribution in member.contributions])
 
-    "Mr Micah": {
-        "full_name": "Mr Micah Ogbuka",
-        "department": "Quality Control",
-        "company": "Royal Salt Limited",
-        "contributions": {"May": 6000, "June": 6000, "July": 6000, "August": 6000, "September": 6000, "October": 6000, "November": 6000, "December": 6000},
-        "total_contribution": 48000
-    },
-    "Mr Fortunate": {
-        "full_name": "Mr Fortunate Ewelike",
-        "department": "Production",
-        "company": "Royal Salt Limited",
-        "contributions": {"May": 6000, "June": 6000, "July": 6000, "August": 6000, "September": 6000, "October": 6000, "November": 6000, "December": 6000},
-        "total_contribution": 48000
-    },
-    "Mr Junior": {
-        "full_name": "Mr Junior Akudiwe",
-        "department": "Quality Control",
-        "company": "Royal Salt Limited",
-        "contributions": {"May": 6000, "June": 6000, "July": 6000, "August": 6000, "September": 6000, "October": 6000, "November": 6000, "December": 6000},
-        "total_contribution": 48000
-    },
-    "Mr Emmanuel": {
-        "full_name": "Mr Emnanuel Inyokwe",
-        "department": "Quality Control",
-        "company": "Royal Salt Limited",
-        "contributions": {"May": 6000, "June": 6000, "July": 6000, "August": 6000, "September": 6000, "October": 6000, "November": 6000, "December": 6000},
-        "total_contribution": 48000
-    },
-        
-    "Mr Anthony": {
-        "full_name": "Mr Emmanuel Anthony",
-        "department": "Quality Control",
-        "company": "Royal Salt Limited",
-        "contributions": {"May": 6000, "June": 6000, "July": 6000, "August": 6000, "September": 6000, "October": 6000, "November": 6000, "December": 6000},
-        "total_contribution": 48000
-    },
-    "Mr Musiliu": {
-        "full_name": "Mr Musiliu Odewale",
-        "department": "Health, Safety and Environment",
-        "company": "Royal Salt Limited",
-        "contributions": {"May": 6000, "June": 6000, "July": 6000, "August": 6000, "September": 6000, "October": 6000, "November": 6000, "December": 6000},
-        "total_contribution": 48000
-    },
-
-    "Mr Donatus": {
-        "full_name": "Mr Donatus Odunze",
-        "department": "Security",
-        "company": "Royal Salt Limited",
-        "contributions": {"May": 6000, "June": 6000, "July": 6000, "August": 6000, "September": 6000, "October": 6000, "November": 6000, "December": 6000},
-        "total_contribution": 48000
-    },
-    "Mr Ikenna": {
-        "full_name": "Mr Ikenna Wilson",
-        "department": "Quality Control",
-        "company": "Royal Salt Limited",
-        "contributions": {"May": 6000, "June": 6000, "July": 6000, "August": 6000, "September": 6000, "October": 6000, "November": 6000, "December": 6000},
-        "total_contribution": 48000
-    },
-    
-}
-
-# Contribution data
-contributions = {
-    "Mr Albert": 48000,
-    "Mrs Jacinta": 48000,
-    "Mr Omini": 48000,
-    "Mrs Henrietta": 48000,
-    "Mr Micah": 48000,
-    "Mr Fortunate": 48000,
-    "Mr Junior": 48000,
-    "Mr Emmanuel": 48000,
-    "Mr Anthony": 48000,
-    "Mr Musiliu": 48000,
-    "Mr Donatus": 48000,
-    "Mr Ikenna": 48000
-}
-
-# Goods and expenses data
-goods_expenses = {
-    "Oil": 85500,
-    "Spaghetti": 120000,
-    "Tomato": 56400,
-    "Semo": 99600,
-    "Idomi": 45000,
-    "Sugar": 8400,
-    "Magi": 12000,
-    "Detergent": 10800,
-    "Peak milk": 28800,
-    "Milo": 21000,
-    "Crayfish": 24000,
-    "Stock fish": 12000,
-    "Expenses": 4200
-}
+def calculate_total_goods_expenses():
+    return session.query(func.sum(Expenses.cost)).scalar() or 0
 
 # Monthly contribution per member
 monthly_contribution = 6000
 
+# Fetch all members from the database
+members = session.query(Member).all()
+
 # Calculate total contribution
-total_contribution = sum(contributions.values())
+total_contribution = sum(calculate_total_contribution(member) for member in members)
 
 # Calculate total goods and expenses
-total_goods_expenses = sum(goods_expenses.values())
+total_goods_expenses = calculate_total_goods_expenses()
 
 # Calculate balance
 balance = total_contribution - total_goods_expenses
+
 
 @app.route('/')
 def summary():
@@ -144,12 +31,12 @@ def summary():
 
 @app.route('/individual/<member_name>')
 def individual(member_name):
-    if member_name in members_data:
-        member_details = members_data[member_name]
-        return render_template('individual.html', full_name=member_details["full_name"],
-                               department=member_details["department"], company=member_details["company"],
-                               contributions=member_details["contributions"],
-                               total_contribution=member_details["total_contribution"])
+    member = session.query(Member).filter_by(full_name=member_name).first()
+    if member:
+        return render_template('individual.html', full_name=member.full_name,
+                               department=member.department, company=member.company,
+                               contributions=member.contributions,
+                               total_contribution=member.total_contribution)
     else:
         return "Member not found"
 if __name__ == '__main__':
