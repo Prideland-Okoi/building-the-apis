54b7443a1591ecd81cb9cf83c0cbe2568e668882
Prideland-Okoi <prideland.okoi@gmail.com>
2024-02-11 00:27:31 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,53 +1,48 @@
 # models.py
 
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
 
-DATABASE_URI = 'coop_db'
-engine = create_engine(DATABASE_URI)
+db = SQLAlchemy()
 
-Base = declarative_base()
 
-class Member(Base):
+class Member(db.Model):
     __tablename__ = 'members'
 
-    id = Column(Integer, primary_key=True)
-    full_name = Column(String)
-    department = Column(String)
-    company = Column(String)
-    total_contribution = Column(Float)
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(60), unique=True, nullable=False)
+    department = db.Column(db.String)
+    company = db.Column(db.String)
 
-    contributions = relationship("Contribution", back_populates="member")
+    contributions = db.relationship("Contribution", back_populates="member")
 
-    def __init__(self, full_name, department, company, contributions):
+    def __init__(self, full_name, department, company):
         self.full_name = full_name
         self.department = department
         self.company = company
-        self.contributions = contributions
-        self.total_contribution = sum(contributions.values())
 
-class Contribution(Base):
+
+class Contribution(db.Model):
     __tablename__ = 'contributions'
 
-    id = Column(Integer, primary_key=True)
-    month = Column(String)
-    amount = Column(Float)
-    member_id = Column(Integer, ForeignKey('members.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    month = db.Column(db.String)
+    amount = db.Column(db.Float)
+    member_id = db.Column(db.Integer, db.ForeignKey('members.id'))
 
-    member = relationship("Member", back_populates="contributions")
+    member = db.relationship("Member", back_populates="contributions")
 
-    def __init__(self, month, amount):
+    def __init__(self, member_id, month, amount):
+        self.member_id = member_id
         self.month = month
         self.amount = amount
 
-class Expenses(Base):
+
+class Expenses(db.Model):
     __tablename__ = 'expenses'
 
-    id = Column(Integer, primary_key=True)
-    item = Column(String)
-    cost = Column(Float)
+    id = db.Column(db.Integer, primary_key=True)
+    item = db.Column(db.String)
+    cost = db.Column(db.Float)
 
     def __init__(self, item, cost):
         self.item = item
